9d7023b8b16357cb51d1b7b2794d8d8b3f2b368a
Tatsuki hata <kisaki@TatsukinoiMac.local>
2026-03-14 01:21:35 +0900

    add shopify toml

---
+++ get_token.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "以下のURLをブラウザのアドレスバーに直接コピペしてください："
+echo ""
+echo "https://nicopuff-jp.myshopify.com/admin/oauth/authorize?client_id=f4891ed8c8ca2351bfe7f0917c494394&scope=write_content,read_content&redirect_uri=https://blogcraftai.vercel.app/api/auth/callback&state=abc123"
